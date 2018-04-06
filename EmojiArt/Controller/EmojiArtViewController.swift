60435589ec1057def2909f52a18ff5ab88b0c943
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-06 10:41:28 +0200

    Prepare for image drag and drop

--- EmojiArt/Controller/EmojiArtViewController.swift
+++ EmojiArt/Controller/EmojiArtViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class EmojiArtViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var emojiArtView: EmojiArtView!
+    @IBOutlet weak var dropZone: UIView! {
+        didSet {
+            dropZone.addInteraction(UIDropInteraction(delegate: self))
+        }
+    }
+    
     // MARK: - View Controller's Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+}
+
+// MARK: - UIDropInteractionDelegate implementation
+extension EmojiArtViewController: UIDropInteractionDelegate {
+    
+    // Only accept something that is an URL and UIImage
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    // We are going to copy image from outside of the app
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    // Perform drop
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSURL.self) { nsurls in
+            
+        }
+        session.loadObjects(ofClass: UIImage.self) { images in 
+            
+        }
+    }
 }
