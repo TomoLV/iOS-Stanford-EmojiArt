aef2eb1185fb821b99798ef091175b2758b0e604
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-06 10:24:29 +0200

    Add EmojiArt VC

---
+++ EmojiArt/Controller/EmojiArtViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Tomasz Bogusz on 06.04.2018.
+//  Copyright © 2018 Tomasz Bogusz. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
