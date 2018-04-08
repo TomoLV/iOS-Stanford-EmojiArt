c1d41c48c992aaa288aff2208ea7a57fa36928da
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-08 14:51:33 +0200

    Add EmojiCollectionViewCell

---
+++ EmojiArt/View/EmojiCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Tomasz Bogusz on 08.04.2018.
+//  Copyright © 2018 Tomasz Bogusz. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var label: UILabel!
+    
+}
